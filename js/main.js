619c8f027e187dca63943cd32a9b2aac18948958
Shaiel Jimenez <sm.jimenezp1@uniandes.edu.co>
2025-04-07 00:13:53 -0500

    Taller 2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 import { series } from './data.js';
 const seriesTbody = document.getElementById('tv-series-list');
 const seriesAverageNum = document.getElementById('seasons-average');
+const seriesCardDetail = document.getElementById('series-details-card');
 renderSeriesTable(series);
 seriesAverageNum.innerHTML = `${getAverageSeasons(series)}`;
 function renderSeriesTable(series) {
     series.forEach(s => {
         let tableElement = document.createElement('tr');
         tableElement.innerHTML = `<td>${s.id}</td>
-                                <td>${s.title}</td>
+                                <td><a href="#" class="series-title" style="cursor:pointer; color:blue;">${s.title}</a></td>
                                 <td>${s.channel}</td>
                                 <td>${s.seasons}</td>`;
+        let seriesTitleElement = tableElement.querySelector('.series-title');
+        seriesTitleElement.addEventListener('click', () => {
+            renderSeriesCard(s);
+        });
         seriesTbody.appendChild(tableElement);
     });
 }
@@ -20,4 +25,12 @@ function getAverageSeasons(series) {
     });
     let totalSeasons = series.length;
     return averageSeasons / totalSeasons;
+}
+function renderSeriesCard(s) {
+    seriesCardDetail.classList.remove('d-none');
+    seriesCardDetail.innerHTML = `
+                                <img src="${s.image}" alt="${s.title}">
+                                <h2 style="padding-top: 1rem">${s.title}</h2>
+                                <p>${s.description}</p>
+                                <a href="${s.url}">Website</a>`;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -4,6 +4,7 @@ import { series } from './data.js';
 
 const seriesTbody: HTMLElement = document.getElementById('tv-series-list')!;
 const seriesAverageNum: HTMLElement = document.getElementById('seasons-average')!;
+const seriesCardDetail: HTMLElement = document.getElementById('series-details-card')!;
 
 renderSeriesTable(series); 
 seriesAverageNum.innerHTML = `${getAverageSeasons(series)}`
@@ -12,9 +13,15 @@ function renderSeriesTable(series: Serie[]): void {
     series.forEach(s => {
         let tableElement = document.createElement('tr');
         tableElement.innerHTML = `<td>${s.id}</td>
-                                <td>${s.title}</td>
+                                <td><a href="#" class="series-title" style="cursor:pointer; color:blue;">${s.title}</a></td>
                                 <td>${s.channel}</td>
                                 <td>${s.seasons}</td>`;
+
+        let seriesTitleElement = tableElement.querySelector('.series-title')!;
+        seriesTitleElement.addEventListener('click', () => {
+            renderSeriesCard(s);
+        });
+
         seriesTbody.appendChild(tableElement);
     });
 }
@@ -26,4 +33,13 @@ function getAverageSeasons(series: Serie[]): number {
     });
     let totalSeasons: number = series.length;
     return averageSeasons / totalSeasons;
+}
+
+function renderSeriesCard(s: Serie): void {
+    seriesCardDetail.classList.remove('d-none');
+    seriesCardDetail.innerHTML = `
+                                <img src="${s.image}" alt="${s.title}">
+                                <h2 style="padding-top: 1rem">${s.title}</h2>
+                                <p>${s.description}</p>
+                                <a href="${s.url}">Website</a>`;
 }
